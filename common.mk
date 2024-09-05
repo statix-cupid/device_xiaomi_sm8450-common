54c3351782bedef64960c14d611602c5e67ebb79
rahul <rahul@aospa.co>
2024-09-05 21:49:05 +0100

    sm8450-common: Migrate to Xiaomi Sensor MultiHAL AIDL

--- common.mk
+++ common.mk
@@ -435,7 +435,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.xiaomi-multihal \
+    android.hardware.sensors-service.xiaomi-multihal \
     android.frameworks.sensorservice@1.0.vendor:64
 
 PRODUCT_PACKAGES += \
