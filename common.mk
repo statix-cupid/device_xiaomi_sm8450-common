8c47aa06c73d4e27dc85450a4f00c7d15462f359
rahul <rahul@aospa.co>
2024-09-12 22:48:32 +0000

    Revert "sm8450-common: Build opensource soundtrigger HAL"
    This reverts commit 58dca940ce2a6f4f59233d72b78ac7bdb9edf16d.

--- common.mk
+++ common.mk
@@ -71,8 +71,7 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default:64 \
     audio.primary.taro:64 \
     audio.r_submix.default:64 \
-    audio.usb.default:64 \
-    sound_trigger.primary.taro:64
+    audio.usb.default:64
 
 PRODUCT_PACKAGES += \
     audioadsprpcd
