ae3c80a55e81a71aa21c69ca8e32576bd4ad8b32
rahul <rahul@aospa.co>
2024-09-23 15:03:12 +0100

    sm8450-common: Switch to prebuilt audio
    This reverts commit 58dca940ce2a6f4f59233d72b78ac7bdb9edf16d.
    This reverts commit 38594cb731c7804c6832575eb6bb0ec0ab6327de.
    This reverts commit ac159cbe7d5e54b0e79dbed26823c41b8bba4b03.
    This reverts commit f7d5baf7698f2e18d127918b795d89af2a7a18a9.
    This reverts commit eb808906a72e405374a6acc9efba603caac79e1b.
    This reverts commit 34c89d972c286caf40393a485d40ddb91fd51c36.
    This reverts commit 97f292d9e1e1a96fd03873b43ded40c47c17a1fa.
    This reverts commit 50b31a4917d78af1cef40d08e7da8cf7aecaf3f8.
    This reverts commit a625efdf5d7c7ba2b53fcb49c488dcb20caccfc4.
    This reverts commit d7703ebe6317be882459474b5a62d37cf34f222e.

--- common.mk
+++ common.mk
@@ -60,34 +60,18 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl:64 \
     android.hardware.audio.effect@7.0-impl:64 \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.3-impl:64 \
-    vendor.qti.hardware.AGMIPC@1.0-service \
-    vendor.qti.hardware.pal@1.0-impl:64
+    android.hardware.soundtrigger@2.3-impl:64
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default:64 \
-    audio.primary.taro:64 \
     audio.r_submix.default:64 \
-    audio.usb.default:64 \
-    sound_trigger.primary.taro:64
+    audio.usb.default:64
 
 PRODUCT_PACKAGES += \
-    audioadsprpcd
-
-PRODUCT_PACKAGES += \
-    libagm_compress_plugin:64 \
-    libagm_mixer_plugin:64 \
-    libagm_pcm_plugin:64 \
-    libagmclient:64 \
-    libagmmixer:64 \
     libaudioroute.vendor:64 \
-    libbatterylistener:64 \
-    libfmpal:64 \
-    libpalclient:64 \
     libqcompostprocbundle:64 \
     libqcomvisualizer:64 \
     libqcomvoiceprocessing:64 \
-    libsndcardparser:64 \
     libtinycompress:64 \
     libvolumelistener:64
 
@@ -443,9 +427,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.3.vendor:64 \
     android.hardware.radio.deprecated@1.0.vendor:64 \
-
-PRODUCT_PACKAGES += \
-    librmnetctl
 
 # Sensors
 PRODUCT_PACKAGES += \
